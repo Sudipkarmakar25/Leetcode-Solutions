da341d89900d93ff4ebb0144ebccdbd81fb6aa30
Sudipkarmakar25 <153487702+Sudipkarmakar25@users.noreply.github.com>
2024-06-07 15:57:15 +0530

    Added

---
+++ Graphs/Course_Schedule.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void topologicalsort(int n, vector<int> &ans, unordered_map<int, list<int>> &adjList) {
+        queue<int> q;
+        unordered_map<int, int> indegree;
+        for (auto i : adjList) {
+            int src = i.first;
+            for (auto nbr : i.second) {
+                indegree[nbr]++;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+        while (!q.empty()) {
+            int front = q.front();
+            q.pop();
+            ans.push_back(front);
+            for (auto nbr : adjList[front]) {
+                indegree[nbr]--;
+                if (indegree[nbr] == 0) {
+                    q.push(nbr);
+                }
+            }
+        }
+    }
+
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int, list<int>> adjList;
+        for (int i = 0; i < prerequisites.size(); i++) {
+            int v = prerequisites[i][0];
+            int u = prerequisites[i][1];
+            adjList[u].push_back(v);
+        }
+
+        vector<int> ans;
+        topologicalsort(numCourses, ans, adjList);
+
+        return ans.size() == numCourses;
+    }
+};
