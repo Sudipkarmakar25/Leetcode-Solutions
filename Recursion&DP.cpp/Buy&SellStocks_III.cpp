366e04adc58389af5b800e639f5f0a16bb0ec648
Sudipkarmakar25 <153487702+Sudipkarmakar25@users.noreply.github.com>
2024-07-20 14:52:58 +0530

    Added

---
+++ Recursion&DP.cpp/Buy&SellStocks_III.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int solve(int index, int buy, vector<int>& prices, vector<vector<vector<int>>>& dp,
+              int limit) {
+        if (index == prices.size()) {
+            return 0;
+        }
+        if (limit == 0) {
+            return 0;
+        }
+        if (dp[index][buy][limit] != -1) {
+            return dp[index][buy][limit];
+        }
+        int profit = 0;
+        if (buy) {
+            profit = max(
+                (-1 * prices[index] + solve(index + 1, 0, prices, dp, limit)),
+                (0 + solve(index + 1, 1, prices, dp, limit)));
+        } else {
+            profit = max(
+                (prices[index] + solve(index + 1, 1, prices, dp, limit - 1)),
+                (0 + solve(index + 1, 0, prices, dp, limit)));
+        }
+        dp[index][buy][limit] = profit;
+        return profit;
+    }
+    
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
+        int limit = 2;
+        return solve(0, 1, prices, dp, limit);
+    }
+};
